7a95a8488569da8ab01ab6ed0dae4ae530b3622a
rafael-escalante <rafaelescalante0607@gmail.com>
2025-03-20 13:54:51 -0600

    Starting DonutTile

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
-  const DonutTab({super.key});
+  //list of donuts
+  final List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
+
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +21,16 @@ class DonutTab extends StatelessWidget {
           //numero de columnas
           crossAxisCount: 2),
       //cuantos elementos
-      itemCount: 2,
+      itemCount: 4,
       padding: const EdgeInsets.all(12),
       //que elemento se construira
       itemBuilder: (context, index) {
-        return;
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
       },
     );
   }
---
+++ lib/utils/donut_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DonutTile extends StatelessWidget {
+  final String donutFlavor;
+  final String donutPrice;
+  //dinamy por que sera del tipo color
+  final dynamic donutColor;
+  final String imageName;
+
+  const DonutTile(
+      {super.key,
+      required this.donutFlavor,
+      required this.donutPrice,
+      required this.donutColor,
+      required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        //color: donutColor[50],
+        decoration: BoxDecoration(
+          color: donutColor[50],
+          //Esquinas redondeadas
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: donutColor[100],
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(24),
+                        topRight: Radius.circular(24)),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                  child: Text(
+                    '\$$donutPrice',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: donutColor[800]),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
